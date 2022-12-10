bb7306ceb492d3dd50aca9c00faed427716c4cc4
foxxywh <115945304+foxxywh@users.noreply.github.com>
2022-12-10 10:52:25 -0700

    Update main_84.js

--- main_84.js
+++ main_84.js
@@ -3,10 +3,10 @@ pizza_list_array = [];
 function getmenu()
 	{    
         // Give the appropriate id name as pizza_name
-        var item = document.getElementById("        ").value; 
+        var item = document.getElementById("pizza_name").value; 
        
         //Add push() function to push item into array
-        pizza_list_array.      (item);
+        pizza_list_array.push(item);
         
         var remove_commas = pizza_list_array.join("<br># ");
         document.getElementById("display_menu").innerHTML =  "# " + remove_commas;
